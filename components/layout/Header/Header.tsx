831b6f06dde58196376f9b89f60bb81f3d534f61
the-papabear <rgabrielursu@gmail.com>
2023-03-07 10:22:15 +0200

    Updates header with going back to root on logo click

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,13 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-import logo from "@/public/logo.png";
+import logo from '@/public/logo.png';
 
-import styles from "@components/layout/Header/Header.module.css";
+import styles from '@components/layout/Header/Header.module.css';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <div className={styles.header__wrapper}>
-      <Image width="48" height="48" alt="movemf logo" src={logo} />
+      <Image
+        src={logo}
+        width="48"
+        height="48"
+        alt="movemf logo"
+        onClick={() => router.push('/')}
+        className={styles.header__logo}
+      />
       <button className={styles.header__btn}>Log in</button>
     </div>
   );
