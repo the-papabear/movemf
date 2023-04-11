e8c8f8ff42532450d477ce6468c3132b48a342a1
Gabriel Ursu <rgabrielursu@gmail.com>
2023-04-11 16:52:14 +0300

    Makes a basic GET request for exercises

--- components/layout/ExercisesCard/ExercisesCard.tsx
+++ components/layout/ExercisesCard/ExercisesCard.tsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
 import Card from '@components/common/Card/Card';
 import Modal from '@components/common/Modal/Modal';
 import ExercisesForm from '@components/layout/ExercisesCard/ExercisesForm/ExercisesForm';
@@ -5,6 +9,12 @@ import ExercisesForm from '@components/layout/ExercisesCard/ExercisesForm/Exerci
 import styles from '@components/layout/ExercisesCard/ExercisesCard.module.css';
 
 const ExercisesCard = () => {
+  const [exercises, setExercises] = useState<any | []>();
+
+  useEffect(() => {
+    axios.get('/api/exercises').then((res) => setExercises(res.data));
+  }, []);
+
   return (
     <Card>
       <div className={styles.exercisesCard__title}>
@@ -13,6 +23,10 @@ const ExercisesCard = () => {
           <ExercisesForm />
         </Modal>
       </div>
+      {exercises &&
+        exercises.map((exercise: any, index: number) => (
+          <span key={index}>{exercise.name}</span>
+        ))}
     </Card>
   );
 };
