6033b77e0fe44073fc3bf54bf98e55abaa9063e9
Gabriel Ursu <rgabrielursu@gmail.com>
2023-05-22 14:00:34 +0300

    Adds next-auth boilerplate

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth, { NextAuthOptions, SessionStrategy } from 'next-auth';
+
+import { mongoClient } from 'backend/mongoConnection';
+import retrieveUserById from 'backend/domain/user/repository/retrieveUserById';
+
+const options: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt' as SessionStrategy,
+  },
+  adapter: MongoDBAdapter(mongoClient()),
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      authorize: async (credentials): Promise<any> => {
+        const user = await retrieveUserById(credentials);
+
+        if (user) {
+          return Promise.resolve(user);
+        } else {
+          return Promise.resolve(null);
+        }
+      },
+      credentials: {
+        email: {
+          type: 'text',
+          label: 'Email',
+          placeholder: 'Type in your email',
+        },
+        password: {
+          type: 'password',
+          label: 'Password',
+          placeholder: 'Type in your password',
+        },
+      },
+    }),
+  ],
+};
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, options);
+}
