b760bb5992ab725f10b2d9b24e84d12b0bc89ad8
Gabriel Ursu <rgabrielursu@gmail.com>
2023-05-29 16:45:43 +0300

    Fixes collection typo

--- backend/domain/workout/repository/retrieveWorkouts.ts
+++ backend/domain/workout/repository/retrieveWorkouts.ts
@@ -3,7 +3,7 @@ import dbConnection from 'backend/mongoConnection';
 const retrieveWorkouts = async () => {
   const db = await dbConnection();
 
-  return await db.collection('workout').find({}).toArray();
+  return await db.collection('workouts').find({}).toArray();
 };
 
 export default retrieveWorkouts;
