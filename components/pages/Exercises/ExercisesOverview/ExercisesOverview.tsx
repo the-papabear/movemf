9e081622902b8a927fc5cd34d7806076e188e96c
Gabriel Ursu <rgabrielursu@gmail.com>
2023-08-14 13:48:44 +0300

    Refactors component to Tailwind CSS

--- components/pages/Exercises/ExercisesOverview/ExercisesOverview.tsx
+++ components/pages/Exercises/ExercisesOverview/ExercisesOverview.tsx
@@ -1,11 +1,10 @@
 import { MoreHorizontal } from 'react-feather';
 
+import { Dialog } from '@/common/Dialog';
 import { Button, Modal } from '@/common';
 import { DropdownMenu } from '@/common/DropdownMenu/DropdownMenu';
 import { ExerciseDTO, ExerciseData } from '@/pages/Exercises/interfaces';
 import { EditExercise } from '@/pages/Exercises/EditExercise/EditExercise';
-
-import styles from '@/pages/Exercises/ExercisesOverview/ExercisesOverview.module.css';
 
 interface ExercisesOverviewProps {
   isModalOpen: boolean;
@@ -27,33 +26,43 @@ export const ExercisesOverview = ({
   setExerciseData,
 }: ExercisesOverviewProps) => {
   return (
-    <div className={styles.exercises__wrapper}>
+    <div className="p-4">
       {exercises &&
         exercises.map((exercise, index) => (
-          <div className={styles.items} key={index}>
+          <div key={index} className="flex align-center gap-4 p-2 rounded hover:bg-gray-300">
             <DropdownMenu trigger={<MoreHorizontal />}>
-              {exercise.link && (
-                <Button>
-                  <a target="_blank" href={`http://${exercise.link}`} className={styles.link}>
-                    Go to link
-                  </a>
-                </Button>
-              )}
-              <Modal
-                open={isModalOpen}
-                title={exercise.name}
-                trigger={<span>Edit</span>}
-                toggleModal={setIsModalOpen(exercise._id, exercise.name, exercise.link!)}
-              >
-                <EditExercise
-                  exerciseData={exerciseData}
-                  submitExercise={submitExercise}
-                  setExerciseData={setExerciseData}
-                />
-              </Modal>
-              <Button onClick={handleDelete(exercise._id)} className={styles['dropdown__item--dark']}>
-                Delete
-              </Button>
+              <>
+                {exercise.link && (
+                  <Button>
+                    <a target="_blank" href={`http://${exercise.link}`}>
+                      Go to link
+                    </a>
+                  </Button>
+                )}
+                <Modal
+                  open={isModalOpen}
+                  title={exercise.name}
+                  trigger={<span>Edit</span>}
+                  toggleModal={setIsModalOpen(exercise._id, exercise.name, exercise.link!)}
+                >
+                  <EditExercise
+                    exerciseData={exerciseData}
+                    submitExercise={submitExercise}
+                    setExerciseData={setExerciseData}
+                  />
+                </Modal>
+                <Dialog trigger={<Button className="bg-gray-700 text-white">Delete</Button>}>
+                  <div className="flex flex-col gap-4">
+                    <span className="text-left">
+                      By deleting this exercise you will delete{' '}
+                      <span className="text-red-500">all workouts that use it!</span> Continue?
+                    </span>
+                    <button className="bg-gray-700 text-white rounded" onClick={handleDelete(exercise._id)}>
+                      Delete
+                    </button>
+                  </div>
+                </Dialog>
+              </>
             </DropdownMenu>
             {exercise.name}
           </div>
