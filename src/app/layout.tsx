0137b64fc6722aa4abe4ef02b5c355436babbd9e
Gabriel Ursu <rgabrielursu@gmail.com>
2023-08-30 11:20:34 +0300

    Updates next auth to work with nextjs 13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,19 @@
 'use client';
 
-import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
-
-import { Header } from '@/components/';
-
 import '@/app/globals.css';
+import { Header } from '@/components/';
+import { NextAuthProvider } from '@/app/providers';
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider session={session}>
+        <NextAuthProvider>
           <div className="flex gap-4 items-center flex-col max-w-5xl m-0 p-4">
             <Header />
-            <Component {...pageProps} />
+            {children}
           </div>
-        </SessionProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
