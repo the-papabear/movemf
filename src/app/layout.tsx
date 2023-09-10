1ae78ce98f48d72730206e7ee76e451dd3232f5c
Gabriel Ursu <rgabrielursu@gmail.com>
2023-09-10 17:04:50 +0300

    Adds new font to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
 'use client';
 
+import { Mulish } from 'next/font/google';
+
 import { Header, Footer } from '@/components/';
 import { NextAuthProvider } from '@/app/providers';
 
 import '@/app/globals.css';
 
+const mulish = Mulish({ subsets: ['latin'], display: 'swap', variable: '--font-main' });
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className={`${mulish.variable} h-full`}>
       <head>
         <title>MoveMF - A workout diary app</title>
       </head>
-      <body className="h-full">
+      <body className="font-main h-full">
         <NextAuthProvider>
           <div className="m-auto flex h-full max-w-5xl flex-col items-center p-4">
             <Header />
