91952fe42130faaedc4517dddb424b07d8903339
Gabriel Ursu <rgabrielursu@gmail.com>
2023-09-24 16:55:53 +0300

    Makes root layout nextjs compliant

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,30 @@
-'use client';
-
+import { PropsWithChildren } from 'react';
 import { Mulish } from 'next/font/google';
-import { SessionProvider } from 'next-auth/react';
 
-import { Header, Footer } from '@/components/';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import { NextAuthProvider } from '@/app/providers';
 
 import '@/app/globals.css';
 
 const mulish = Mulish({ subsets: ['latin'], display: 'swap', variable: '--font-main' });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: 'Movemf - Minimalist workout diary',
+  description: 'A minimalist workout diary app',
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" className={`${mulish.variable} h-full`}>
-      <head>
-        <title>MoveMF - A workout diary app</title>
-      </head>
       <body className="h-full font-main">
-        <SessionProvider>
+        <NextAuthProvider>
           <div className="m-auto flex h-full max-w-5xl flex-col items-center p-4">
             <Header />
             <div className="w-full flex-1">{children}</div>
             <Footer />
           </div>
-        </SessionProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
