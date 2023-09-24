54a24f9ea433d19533e1f038d8e2273f3a0c7b35
Gabriel Ursu <rgabrielursu@gmail.com>
2023-09-24 22:33:33 +0300

    Refactors the Exercise overview page

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -1,32 +1,32 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-
 import axios from 'axios';
 import Link from 'next/link';
-import dynamic from 'next/dynamic';
-import { MoreHorizontal } from 'react-feather';
+import { Edit2, Link2, Trash2 } from 'react-feather';
 
-import { Button, Dialog, DropdownMenu } from '@/components';
+import Button from '@/components/Button';
 import { CreateOrEditExerciseProps, ExerciseData, ExerciseDTO } from '@/app/exercises/interfaces';
 
-function ExercisesPage() {
-  const [refetchData, setRefetchData] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+export default function Exercises() {
+  const [isCreateFormOpen, setIsCreateFormOpen] = useState(false);
+  const [isEditFormOpen, setIsEditFormOpen] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
   const [exerciseData, setExerciseData] = useState<ExerciseData>({ id: '', name: '', link: '' });
 
   useEffect(() => {
     const getExercises = async () => {
-      const res = await axios.get('/api/exercises');
+      const { data } = await axios.get('/api/exercises');
 
-      setExercises(res.data.data);
+      setExercises(data.data);
+      setIsLoading(false);
     };
 
     getExercises();
-  }, [refetchData]);
+  }, []);
 
   const handleChange = (event: any) => {
     const { name, value } = event.target;
@@ -37,134 +37,129 @@ function ExercisesPage() {
     e.preventDefault();
 
     if (exerciseData.id) {
-      axios.patch(`/api/exercises/${exerciseData.id}`, exerciseData);
+      setIsEditFormOpen(!isEditFormOpen);
+
+      axios.patch(`/api/exercises/${exerciseData.id}`, exerciseData).then((data) => {
+        const filteredData = exercises.filter((exercise) => exercise._id !== exerciseData.id);
+        setExercises([{ ...data.data.data }, ...filteredData]);
+      });
     } else {
-      axios.post('/api/exercises', exerciseData);
+      axios.post('/api/exercises', exerciseData).then((data) => setExercises([...exercises, { ...data.data.data }]));
     }
 
     setExerciseData({ id: '', name: '', link: '' });
-    setIsModalOpen(false);
-    setIsEditModalOpen(false);
-    setRefetchData(!refetchData);
+    setIsCreateFormOpen(false);
   };
 
   const handleDelete = (exerciseId: string) => async () => {
-    await axios.delete(`/api/exercises/${exerciseId}`);
+    const data = exercises.filter((exercise) => exercise._id !== exerciseId);
+    setExercises(data);
 
-    setRefetchData(!refetchData);
+    await axios.delete(`/api/exercises/${exerciseId}`);
+  };
+
+  const handleAddBtnClick = () => {
+    setExerciseData({ id: '', name: '', link: '' });
+    setIsCreateFormOpen(!isCreateFormOpen);
+    setIsEditFormOpen(false);
   };
 
   return (
-    <>
-      <Dialog
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
-        trigger={
-          <Button onClick={() => setExerciseData({ id: '', name: '', link: '' })} className="my-8 h-8">
-            Add Exercise
-          </Button>
-        }
-      >
+    <div className="flex flex-col items-center gap-2">
+      <Button title={isCreateFormOpen || isEditFormOpen ? 'Cancel' : 'Add Exercise'} onClick={handleAddBtnClick} />
+
+      {isCreateFormOpen ? (
         <CreateOrEditExercise exerciseData={exerciseData} handleChange={handleChange} submitExercise={handleSubmit} />
-      </Dialog>
+      ) : null}
+      {isEditFormOpen ? (
+        <CreateOrEditExercise exerciseData={exerciseData} handleChange={handleChange} submitExercise={handleSubmit} />
+      ) : null}
 
       <table className="w-full">
         <thead>
           <tr className="rounded border-b-[1px] border-lime-500">
             <th className="text-left text-xs font-light">Name</th>
-            <th className="text-right text-xs font-light">Actions</th>
+            <th className="pr-4 text-right text-xs font-light">Actions</th>
           </tr>
         </thead>
         <tbody>
-          {exercises.map((exercise, index) => (
-            <tr key={index} className="h-[60px] hover:bg-lime-100">
-              <td>{exercise.name}</td>
-              <td>
-                <div className="flex justify-end pr-3">
-                  <DropdownMenu trigger={<MoreHorizontal width={20} height={20} />}>
-                    <>
-                      {exercise.link && (
-                        <Link
-                          href={`https://${exercise.link}`}
-                          target="_blank"
-                          className="group relative flex h-[25px] cursor-pointer select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none text-gray-500 outline-none hover:bg-lime-200 hover:text-gray-600"
-                        >
-                          Go to link
-                        </Link>
-                      )}
-                      <Dialog
-                        title={exercise.name}
-                        isModalOpen={isEditModalOpen}
-                        setIsModalOpen={setIsEditModalOpen}
-                        trigger={
-                          <div
-                            onClick={() => {
-                              setExerciseData({ id: exercise._id, name: exercise.name, link: exercise.link });
-                              setIsEditModalOpen(true);
-                            }}
-                            className="group relative flex h-[25px] cursor-pointer select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none text-gray-500 outline-none hover:bg-lime-200 hover:text-gray-600"
-                          >
-                            Edit
-                          </div>
-                        }
-                      >
-                        <CreateOrEditExercise
-                          exerciseData={exerciseData}
-                          handleChange={handleChange}
-                          submitExercise={handleSubmit}
-                        />
-                      </Dialog>
-                      <div
-                        onClick={handleDelete(exercise._id)}
-                        className="group relative flex h-[25px] cursor-pointer select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none text-gray-500 outline-none hover:bg-lime-200 hover:text-gray-600 "
-                      >
-                        Delete
-                      </div>
-                    </>
-                  </DropdownMenu>
-                </div>
+          {isLoading ? (
+            <tr>
+              <td className="pt-4">
+                <span className="text-xl">Loading exercises...</span>
               </td>
             </tr>
-          ))}
+          ) : (
+            exercises.map((exercise, index) => (
+              <tr key={index} className="h-[50px] hover:bg-lime-100">
+                <td>
+                  {exercise.link ? (
+                    <Link target="_blank" href={'https://' + exercise.link} className="flex items-center gap-2">
+                      {exercise.name} <Link2 width={16} height={16} />
+                    </Link>
+                  ) : (
+                    <span>{exercise.name}</span>
+                  )}
+                </td>
+                <td className="flex justify-end gap-6 p-4">
+                  <Edit2
+                    width={16}
+                    height={16}
+                    onClick={() => {
+                      setExerciseData({ id: exercise._id, ...exercise });
+                      setIsEditFormOpen(!isEditFormOpen);
+                    }}
+                    className="cursor-pointer text-gray-700 hover:text-gray-500"
+                  />
+                  <Trash2
+                    width={16}
+                    height={16}
+                    className="cursor-pointer text-red-600 hover:text-red-400"
+                    onClick={handleDelete(exercise._id)}
+                  />
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
-    </>
+    </div>
   );
 }
 
 function CreateOrEditExercise({ exerciseData, submitExercise, handleChange }: CreateOrEditExerciseProps) {
   return (
-    <>
+    <div className="w-[280px] rounded-md border border-lime-500 p-4">
       <form className="flex flex-col gap-2" onSubmit={submitExercise}>
         <label htmlFor="name">
           <h4>Name</h4>
         </label>
         <input
           required
+          id="name"
           type="text"
           name="name"
-          value={exerciseData?.name || ''}
+          autoComplete="name"
           onChange={handleChange}
+          value={exerciseData?.name || ''}
           className="h-[30px] rounded border border-lime-700"
         />
         <label htmlFor="link">
           <h4>Link</h4>
         </label>
         <input
+          id="link"
           type="text"
           name="link"
+          autoComplete="link"
           value={exerciseData?.link}
           onChange={handleChange}
           className="h-[30px] rounded border border-lime-700"
         />
-        <Button type="submit" onClick={() => submitExercise} className="mt-8 self-center">
-          Save exercise
-        </Button>
+        <div className="self-center">
+          <Button title="Save exercise" type="submit" onClick={() => submitExercise} />
+        </div>
       </form>
-    </>
+    </div>
   );
 }
-
-const Exercises = dynamic(() => Promise.resolve(ExercisesPage), { ssr: false });
-
-export default Exercises;
