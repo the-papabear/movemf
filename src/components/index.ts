1a71d432ab8e3005fc764128dd2b5e2e709a7aee
Gabriel Ursu <rgabrielursu@gmail.com>
2023-09-24 16:58:21 +0300

    Removes unused components

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '@/components/Button';
-export * from '@/components/Dialog';
-export * from '@/components/DropdownMenu';
-export * from '@/components/DropdownMenuItem';
-export * from '@/components/Footer';
-export * from '@/components/Header';
