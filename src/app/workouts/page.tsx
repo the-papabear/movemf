2e6a342a2f17dff86d1c05a7a195d0348c5cb4a0
Gabriel Ursu <rgabrielursu@gmail.com>
2023-09-24 16:58:46 +0300

    Adds workouts overview UI

---
+++ src/app/workouts/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+
+import Button from '@/components/Button';
+
+export default function Workouts() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    (async function getWorkouts() {
+      const { data } = await axios.get('/api/workouts');
+      setWorkouts(data.data);
+    })();
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Link href="/new-workout">
+        <Button title="Add Workout" />
+      </Link>
+
+      <table className="w-full">
+        <thead>
+          <tr className="rounded border-b-[1px] border-lime-500">
+            <th className="text-left text-xs font-light">Name</th>
+            <th className="text-right text-xs font-light">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {workouts.map((workout: any, index: number) => (
+            <tr key={index} className="h-[60px] cursor-pointer hover:bg-lime-100">
+              <td>{workout.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
