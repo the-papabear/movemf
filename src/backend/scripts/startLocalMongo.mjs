af4f3f2a3072db9bb0c94c6eab25895904c8e5a1
Gabriel R. Ursu <rgabrielursu@gmail.com>
2023-10-08 14:41:58 +0300

    Hardcodes mongo port in script

--- src/backend/scripts/startLocalMongo.mjs
+++ src/backend/scripts/startLocalMongo.mjs
@@ -1,7 +1,3 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
 import { runCommand } from './runCommand.mjs';
 
 async function run() {
@@ -10,7 +6,7 @@ async function run() {
   async function startMongoDBInstance() {
     const cmd = `
       docker run -dit --rm --name mongodb \
-        -p ${process.env.MONGODB_PORT}:${process.env.MONGODB_PORT} \
+        -p 27017:27017 \
         mongo --replSet rs0 --bind_ip 0.0.0.0 && \
       sleep 2 && \
       docker exec mongodb mongosh --eval "rs.initiate({ _id: \\"rs0\\", members: [{ _id: 0, host: \\"localhost:27017\\" }] });"
