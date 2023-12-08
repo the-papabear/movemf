7b2f2fa434e1e78354f6fe6bd88a7eaadf739c29
Gabriel 'The Bear' Ursu <rgabrielursu@gmail.com>
2023-12-09 00:25:18 +0200

    Implements exercise overview page

--- src/app/(protected)/exercises/page.tsx
+++ src/app/(protected)/exercises/page.tsx
@@ -1,162 +1,48 @@
-'use client';
+import { Fragment } from 'react';
+import { ExternalLink, Pen, PlusIcon, Trash2 } from 'lucide-react';
 
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Link from 'next/link';
-import { Edit2, Link2, Trash2 } from 'react-feather';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardTitle } from '@/components/ui/card';
 
-import { CreateOrEditExerciseProps, ExerciseData, ExerciseDTO } from '@/app/(protected)/exercises/interfaces';
+const mockExercises = [
+  { _id: '1', name: 'Lateral extensions', link: 'link.test.com' },
+  { _id: '2', name: 'Long exercise name in this input', link: 'link.test.com' },
+  { _id: '3', name: 'Lateral extensions', link: 'link.test.com' },
+  { _id: '4', name: 'Lateral extensions', link: 'link.test.com' },
+  { _id: '5', name: 'Lateral extensions', link: 'link.test.com' },
+];
 
-const Exercises = () => {
-  const [isCreateFormOpen, setIsCreateFormOpen] = useState(false);
-  const [isEditFormOpen, setIsEditFormOpen] = useState(false);
-
-  const [isLoading, setIsLoading] = useState(true);
-
-  const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
-  const [exerciseData, setExerciseData] = useState<ExerciseData>({ id: '', name: '', link: '' });
-
-  useEffect(() => {
-    const getExercises = async () => {
-      const { data } = await axios.get('/api/exercises');
-
-      setExercises(data.data);
-      setIsLoading(false);
-    };
-
-    getExercises();
-  }, []);
-
-  const handleChange = (event: any) => {
-    const { name, value } = event.target;
-    setExerciseData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  };
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-
-    if (exerciseData.id) {
-      setIsEditFormOpen(!isEditFormOpen);
-
-      axios.patch(`/api/exercises/${exerciseData.id}`, exerciseData).then((data) => {
-        const filteredData = exercises.filter((exercise) => exercise._id !== exerciseData.id);
-        setExercises([{ ...data.data.data }, ...filteredData]);
-      });
-    } else {
-      axios.post('/api/exercises', exerciseData).then((data) => setExercises([...exercises, { ...data.data.data }]));
-    }
-
-    setExerciseData({ id: '', name: '', link: '' });
-    setIsCreateFormOpen(false);
-  };
-
-  const handleDelete = (exerciseId: string) => async () => {
-    const data = exercises.filter((exercise) => exercise._id !== exerciseId);
-    setExercises(data);
-
-    await axios.delete(`/api/exercises/${exerciseId}`);
-  };
-
-  const handleAddBtnClick = () => {
-    setExerciseData({ id: '', name: '', link: '' });
-    setIsCreateFormOpen(!isCreateFormOpen);
-    setIsEditFormOpen(false);
-  };
-
+const ExercisesPage = () => {
   return (
-    <div className="flex flex-col items-center gap-2">
-      {isCreateFormOpen ? (
-        <CreateOrEditExercise exerciseData={exerciseData} handleChange={handleChange} submitExercise={handleSubmit} />
-      ) : null}
-      {isEditFormOpen ? (
-        <CreateOrEditExercise exerciseData={exerciseData} handleChange={handleChange} submitExercise={handleSubmit} />
-      ) : null}
+    <section className="flex w-full flex-col items-center gap-4 py-4">
+      <Button className="w-full">
+        <PlusIcon /> Add new exercise
+      </Button>
 
-      <table className="w-full">
-        <thead>
-          <tr className="rounded border-b-[1px] border-lime-500">
-            <th className="text-left text-xs font-light">Name</th>
-            <th className="pr-4 text-right text-xs font-light">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
-            <tr>
-              <td className="pt-4">
-                <span className="text-xl">Loading exercises...</span>
-              </td>
-            </tr>
-          ) : (
-            exercises.map((exercise, index) => (
-              <tr key={index} className="h-[50px] hover:bg-lime-100">
-                <td>
-                  {exercise.link ? (
-                    <Link target="_blank" href={'https://' + exercise.link} className="flex items-center gap-2">
-                      {exercise.name} <Link2 width={16} height={16} />
-                    </Link>
-                  ) : (
-                    <span>{exercise.name}</span>
-                  )}
-                </td>
-                <td className="flex justify-end gap-6 p-4">
-                  <Edit2
-                    width={16}
-                    height={16}
-                    onClick={() => {
-                      setExerciseData({ id: exercise._id, ...exercise });
-                      setIsEditFormOpen(!isEditFormOpen);
-                    }}
-                    className="cursor-pointer text-gray-700 hover:text-gray-500"
-                  />
-                  <Trash2
-                    width={16}
-                    height={16}
-                    className="cursor-pointer text-red-600 hover:text-red-400"
-                    onClick={handleDelete(exercise._id)}
-                  />
-                </td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </table>
-    </div>
+      <div className="flex w-full flex-col gap-4">
+        {mockExercises.map((exercise) => (
+          <Fragment key={exercise._id}>
+            <Card className="flex flex-col gap-6 p-4">
+              <CardTitle className="truncate">{exercise.name}</CardTitle>
+              <CardContent className="flex items-center justify-between p-0">
+                <Button size="sm" className="flex items-center gap-2">
+                  <ExternalLink /> Exercise Info
+                </Button>
+                <div className="flex gap-4">
+                  <Button size={'sm'}>
+                    <Pen />
+                  </Button>
+                  <Button variant="destructive" size={'sm'}>
+                    <Trash2 />
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          </Fragment>
+        ))}
+      </div>
+    </section>
   );
 };
 
-function CreateOrEditExercise({ exerciseData, submitExercise, handleChange }: CreateOrEditExerciseProps) {
-  return (
-    <div className="w-[280px] rounded-md border border-lime-500 p-4">
-      <form className="flex flex-col gap-2" onSubmit={submitExercise}>
-        <label htmlFor="name">
-          <h4>Name</h4>
-        </label>
-        <input
-          required
-          id="name"
-          type="text"
-          name="name"
-          autoComplete="name"
-          onChange={handleChange}
-          value={exerciseData?.name || ''}
-          className="h-[30px] rounded border border-lime-700"
-        />
-        <label htmlFor="link">
-          <h4>Link</h4>
-        </label>
-        <input
-          id="link"
-          type="text"
-          name="link"
-          autoComplete="link"
-          value={exerciseData?.link}
-          onChange={handleChange}
-          className="h-[30px] rounded border border-lime-700"
-        />
-        <div className="self-center"></div>
-      </form>
-    </div>
-  );
-}
-
-export default Exercises;
+export default ExercisesPage;
