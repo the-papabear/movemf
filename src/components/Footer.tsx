5c043a067d5b84902fdabfbfa4d5c418842c4e85
Gabriel 'The Bear' Ursu <rgabrielursu@gmail.com>
2023-12-08 14:57:28 +0200

    Updates footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { GitHub, Linkedin } from 'react-feather';
 
 export default function Footer() {
   return (
-    <footer className="flex w-full flex-col">
-      <span className="h-[1px] rounded border-b-[1px] border-lime-500" />
+    <footer className="absolute inset-x-0 bottom-0 flex w-full flex-col">
+      <span className="h-[1px] rounded border-b-[1px] border-slate-200" />
       <div className="flex flex-col items-center gap-2 p-2">
         <span className="text-xs text-gray-400">MoveMF - Minimalist workout diary app</span>
         <div className="flex gap-8">
